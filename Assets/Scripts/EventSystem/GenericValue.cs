9ae3e1ce9e605c50b38c6f2ab9507dd383364bd9
linus@windows <linus@windows.de>
2025-01-27 12:38:40 +0100

    lesson till the short break

---
+++ Assets/Scripts/EventSystem/GenericValue.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace EventSystem
+{
+    [CreateAssetMenu(fileName = "GenericValue", menuName = "Scripables/ScripableEvent/GenericData")]
+    public class GenericValue : BaseValue<object>
+    {
+        public enum ValueType
+        {
+            Bool,
+            Int
+        }
+
+        [SerializeField] private ValueType valueType = ValueType.Bool;
+        [SerializeField] private bool boolVal;
+        [SerializeField] private int intVal = 0;
+
+        public override object Value
+        {
+            get
+            {
+                return valueType switch
+                {
+                    ValueType.Bool => boolVal,
+                    ValueType.Int => intVal,
+                    _ => null 
+                };
+            }
+        }
+    }
+}

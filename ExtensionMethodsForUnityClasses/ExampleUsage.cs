f716770224443ae07dba9f59b6696db5f29cf6b6
here.de <lab@here.de>
2025-02-14 20:55:36 +0100

    clean up

---
+++ ExtensionMethodsForUnityClasses/ExampleUsage.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ExampleUsage : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    
+    [Button("Log to Console")]
+    private void TestLoggingToConsole()
+    {
+        //To Unity Console
+        gameObject.LogInfo();
+
+        "hello".Log();
+        1.Log();
+        42.0.Log();
+        gameObject.Log();
+    }
+
+    [Button("Log to Temp File")]
+    public void TestLoggingToFile()
+    {
+        //To temp.txt
+        "Hello file writer!".WriteToTempFile();
+        42.WriteToTempFile();
+        gameObject.name.WriteToTempFile();
+    }
+
+  
+
+    [InfoBox(" With this Example Class you can try all the \n" +
+             " Unity Extension methods, included in the Package.")]
+    [Button("Save GameObject[] to Dictionary")]
+    private void TestArrayToDict()
+    {
+        var newDict = gameObjects.ArrayToDict();
+        foreach (var o in newDict)
+        {
+            Debug.Log(o.Key + "\n" + o.Value);
+        }
+
+        GameObject markObject = newDict["momo"];
+        markObject.SetActive(false);
+    }
+}

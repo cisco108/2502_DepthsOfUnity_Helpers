aab1ae1a889910e007372d6308a2ef11e015064b
here.de <lab@here.de>
2025-02-14 17:48:28 +0100

    added logging extensions

--- Assets/Utillity_pack/ExtensionMethodsForUnityClasses/ExampleUsage.cs
+++ Assets/Utillity_pack/ExtensionMethodsForUnityClasses/ExampleUsage.cs
@@ -6,6 +6,23 @@ public class ExampleUsage : AutoInitPrivateComponentFields
 
     private void Start()
     {
+      TestLoggingExtensions(); 
+       
+    }
+
+    private void TestLoggingExtensions()
+    {
+        //To Unity Console
        gameObject.LogInfo(); 
+       
+       "hello".Log();
+       1.Log();
+       42.0.Log();
+       gameObject.Log();
+        
+       //To temp.txt
+       "Hello file writer!".WriteToFile();
+       42.WriteToFile();
+       gameObject.name.WriteToFile();
     }
 }
--- Assets/Utillity_pack/ExtensionMethodsForUnityClasses/ExtensionMethods.cs
+++ Assets/Utillity_pack/ExtensionMethodsForUnityClasses/ExtensionMethods.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
+using System.IO;
+
 public static class ExtensionMethods
 {
-    
-        
     public static T GetOrAddComponent<T>(this GameObject gameObject) where T : MonoBehaviour
     {
         var component = gameObject.GetComponent<T>();
@@ -17,7 +17,7 @@ public static class ExtensionMethods
                   $"Position: {gO.transform.position} \n" +
                   $"Active: {gO.activeSelf}");
     }
-    
+
     public static void LookAtY(this Transform transform, Vector3 target)
     {
         Vector3 targetPosition = new Vector3(target.x, transform.position.y, target.z);
@@ -26,4 +26,14 @@ public static class ExtensionMethods
         transform.eulerAngles = new Vector3(0, eulerAngles.y, 0);
     }
 
+    public static void Log(this object o)
+    {
+        Debug.Log(o);
+    }
+
+    public static void WriteToFile(this object o)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "temp.txt");
+        File.AppendAllLines(path, new[] { o.ToString() });
+    }
 }

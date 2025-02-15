9cbd302bf8e245ab9bccfc21f164a801584dde18
linus@windows <linus@windows.de>
2025-02-15 09:42:17 +0100

    added basic spawner component

---
+++ Assets/Submission_Package/Helper_Components/ObjectPlacer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    public Transform placeLocation;
+    public GameObject spawn;
+    public List<GameObject> objectsToPlace;
+
+    [Button("Spawn")]
+    public void Sers()
+    {
+        Instantiate(spawn, placeLocation.position, placeLocation.rotation);
+    }
+}

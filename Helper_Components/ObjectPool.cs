47320a9f635c12c3fce1fa5a361315e9642a88c9
here.de <lab@here.de>
2025-02-15 18:43:54 +0100

    added object pool component

---
+++ Helper_Components/ObjectPool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObjectPool
+{
+    private List<GameObject> _pooledObjects;
+    private int _poolsize;
+    private GameObject _prefab;
+    private bool _isPoolEmpty = true;
+
+    public ObjectPool(GameObject prefab, int poolsize)
+    {
+        _prefab = prefab;
+        _poolsize = poolsize;
+        _pooledObjects = new();
+
+        for (int i = 0; i < _poolsize; i++)
+        {
+            GameObject gameObject = GameObject.Instantiate(_prefab) as GameObject;
+            gameObject.SetActive(false);
+            _pooledObjects.Add(gameObject);
+            //Debug.Log($"objects stored: {_pooledObjects.Count}");
+        }
+    }
+
+    public GameObject GetObject(Transform caller)
+    {
+        GameObject gameObject = null;
+        try
+        {
+            gameObject = _pooledObjects.First(i => !i.activeInHierarchy);
+        }
+        catch
+        {
+            gameObject = AddOneMore();
+        }
+
+        gameObject.transform.position = caller.position;
+        gameObject.transform.rotation = caller.rotation;
+        gameObject.SetActive(true);
+        return gameObject;
+    }
+
+    GameObject AddOneMore()
+    {
+        _pooledObjects.Add(GameObject.Instantiate(_prefab));
+        return _pooledObjects.Last();
+    }
+}

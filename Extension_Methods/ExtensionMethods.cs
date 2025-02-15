98e18302cbc711d01fb23757bc6e61ce5c854530
here.de <lab@here.de>
2025-02-15 19:19:48 +0100

    left

--- Extension_Methods/ExtensionMethods.cs
+++ Extension_Methods/ExtensionMethods.cs
@@ -18,25 +18,19 @@ public static class ExtensionMethods
 
     public static void LookAtCamera(this Transform transform)
     {
-       transform.LookAt(Camera.main.transform); 
+        transform.LookAt(Camera.main.transform);
     }
+
     public static void ResetRotation(this Transform transform)
     {
         transform.rotation = Quaternion.identity;
     }
+
     public static void LogInfo(this GameObject gO)
     {
         Debug.Log($"Name: {gO.name} \n" +
                   $"Position: {gO.transform.position} \n" +
                   $"Active: {gO.activeSelf}");
-    }
-
-    public static void LookAtY(this Transform transform, Vector3 target)
-    {
-        Vector3 targetPosition = new Vector3(target.x, transform.position.y, target.z);
-        transform.LookAt(targetPosition);
-        Vector3 eulerAngles = transform.eulerAngles;
-        transform.eulerAngles = new Vector3(0, eulerAngles.y, 0);
     }
 
     public static void PrintJoke(this GameObject gameObject)
@@ -69,11 +63,19 @@ public static class ExtensionMethods
 
 
     /////////////////// From the Lectures /////////////////////////////////////
-
     public static T GetOrAddComponent<T>(this GameObject gameObject) where T : MonoBehaviour
     {
         var component = gameObject.GetComponent<T>();
         if (!component) gameObject.AddComponent<T>();
         return component;
+    }
+
+    /////////////////// From Perplexity.ai /////////////////////////////////////
+    public static void LookAtY(this Transform transform, Vector3 target)
+    {
+        Vector3 targetPosition = new Vector3(target.x, transform.position.y, target.z);
+        transform.LookAt(targetPosition);
+        Vector3 eulerAngles = transform.eulerAngles;
+        transform.eulerAngles = new Vector3(0, eulerAngles.y, 0);
     }
 }
---
+++ Helper_Components/ParentingHelper.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ParentingHelper : MonoBehaviour
+{
+    private void CatchChildren()
+    {
+       Physics.SphereCast() 
+    }
+}
